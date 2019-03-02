569a251fb12c92ac3a568842910f060ca8382ac5
iqbalmuttaqin <iqbalmuttaqin76@gmail.com>
2019-03-02 22:04:47 +0700

    ini commit pertama Logout #5

--- app/src/main/java/com/example/asus/bagianiqbal/MainActivity.java
+++ app/src/main/java/com/example/asus/bagianiqbal/MainActivity.java
@@ -38,7 +38,7 @@ public class MainActivity extends AppCompatActivity
     }
 
 
-
+//
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
