d83687db2fd75ea9dc0d141b7c7a4e7b1f3e256e
bagasnugrohoadi <bagasnugroho5@gmail.com>
2019-04-19 14:18:13 +0700

    ini commit finish #8

---
+++ app/src/main/java/com/example/newfeedback/LihatBalasan.java
@@ -0,0 +1,14 @@
+package com.example.newfeedback;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LihatBalasan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_balasan);
+    }
+}
+//
